9d1816a5e582133c73093e1886dfe2fc0cc025d3
Md. Abdul Rafeu <rafiu.diu@gmail.com>
2026-09-16 00:45:49 +0600

    update

--- myapp/models.py
+++ myapp/models.py
@@ -1,23 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
 class Info(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
     email = models.EmailField()
     age = models.PositiveIntegerField()
-    
+
+    def __str__(self):
+        return self.name
+
+
 class Course(models.Model):
     title = models.CharField(max_length=100)
     instructor = models.CharField(max_length=100)
     description = models.TextField()
     duration = models.CharField(max_length=100)
-    
+
+    def __str__(self):
+        return self.title
+
+
 class Students(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Student Full Name")
     email = models.EmailField()
     age = models.PositiveIntegerField()
     phone = models.CharField(max_length=100)
     address = models.TextField()
-        
-        
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
